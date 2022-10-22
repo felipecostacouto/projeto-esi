5c9a1f740a00ffec898d203abd5ef8b7773612c8
KenzoOuchi <ck137@hotmail.com>
2022-10-22 20:45:20 -0300

    Added Validation for Name
    root is a reserved name

--- app/models/diretorio.rb
+++ app/models/diretorio.rb
@@ -2,4 +2,6 @@ class Diretorio < ApplicationRecord
   validates :name, presence: { message: 'É obrigatório informar o nome!'}
   validates :name, format: { :with => /\A[a-zA-Z0-9 ]+[a-zA-Z0-9 ]+[a-zA-Z0-9 ]\Z/,
     :message => 'Nome inválido. Não é permitido a utilização dos caracteres especiais'}
+  validates :name, format: { :with => /\A(?!root$)\Z/,
+                             :message => 'Nome reservado. root é o nome para o diretório raiz'}
 end
