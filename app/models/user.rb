2cda32d84db8948903d4c66f9dd7fe78374bbd52
Henrique <henrytavares@usp.br>
2022-11-09 14:28:30 -0300

    fixing/creating tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: { message: "É obrigatório informar o nome!" }
+    validates :email, presence: { message: "É obrigatório informar o email!" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Formato de e-mail inválido!" }
+    validates :password, presence: { message: "É obrigatório informar uma senha!" }
+    validates_presence_of :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'invalido sem nome' do
+    user = User.new(email: "teste@gmail.com")
+    expect(user).not_to be_valid
+  end
+
+  it 'invalido com email com formato invalido' do
+    a = User.new
+    a.name= "joao" 
+    a.email = "a,@gmail.com"    
+    expect(a).not_to be_valid
+  end
+
 end
