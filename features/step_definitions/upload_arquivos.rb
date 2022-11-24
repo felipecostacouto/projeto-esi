c6716a13d8ba5fefed7d0350035524e0c0b5351e
fabriciolk <fabriciokashino@live.com>
2022-11-24 00:08:31 -0300

    Added user history to upload file

---
+++ features/step_definitions/upload_arquivos.rb
@@ -0,0 +1,35 @@
+When(/^que estou na página de um diretório específico$/) do
+  pending
+end
+
+When(/^arrasto um arquivo para onde estão os diretórios$/) do
+  pending
+end
+
+When(/^deverá aparecer o arquivo na lista de arquivos dentro do diretório que estou$/) do
+  pending
+end
+
+When(/^o arquivo é muito grande$/) do
+  pending
+end
+
+When(/^deve aparecer mensagem dizendo que o arquivo é muito grande$/) do
+  pending
+end
+
+When(/^o arquivo tem extensão inválida$/) do
+  pending
+end
+
+When(/^deve aparecer mensagem dizendo que o arquivo não tem extensão válida$/) do
+  pending
+end
+
+When(/^já existe outro arquivo com o mesmo nome naquele diretório$/) do
+  pending
+end
+
+When(/^deve aparecer mensagem perguntando se sobrescreve ou manter o arquivo$/) do
+  pending
+end
