0fa83d9ca2660c1bddbf84ec109e223e571f0939
Cesar Ouchi <ck137@hotmail.com>
2022-12-07 15:34:14 -0300

    Pending Visualizar Campus

---
+++ features/step_definitions/visualizar_campus_step.rb
@@ -0,0 +1,13 @@
+Quando('seleciono {string} no menu') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('uma lista de todos os campi será mostrada') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('um aviso de que não há campi disponíveis deve aparecer') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
