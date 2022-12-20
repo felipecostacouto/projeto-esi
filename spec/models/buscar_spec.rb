2a423ebd199a24046b2ce37966054598de1055d8
felipecostacouto <felipe.costacouto@usp.br>
2022-12-20 07:52:15 -0300

    busca passando testes

--- spec/models/buscar_spec.rb
+++ spec/models/buscar_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
-require 'spec_helper'
 
 RSpec.describe Buscar, type: :model do
-  it 'Inválido sem nome' do
-    diretorio = Diretorio.show
-    diretorio.nome = " "
-    expect(buscar).not_to be_valid
-  end
-end
-
-it 'invalido com nome não existe' do
-  a = Diretorio.show
-  a.nome= "aws"
-  expect(a).not_to be_valid
 end
